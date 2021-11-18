96ff72e693d8b2694bfa35cf1a83b88bb6373cad
Jonathan LAURENT <johjo@ytreza.org>
2021-11-18 05:47:41 +0100

    red : create test test_return_sum_if_two_numbers_when_calculate

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -11,3 +11,7 @@ def test_return_zero_if_empty_string_when_calculate():
 def test_return_number_if_one_number_when_calculate():
     assert calculate("54") == 54
     assert calculate("107") == 107
+
+
+def test_return_sum_if_two_numbers_when_calculate():
+    assert calculate("12,124") == 136
