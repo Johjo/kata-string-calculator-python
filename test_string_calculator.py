f0c17d350708a6701eafec933aea2fb57f1811c3
Jonathan LAURENT <johjo@ytreza.org>
2021-11-18 06:03:23 +0100

    refactoring : 3 = i + 1

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -10,7 +10,7 @@ def calculate(expression):
     if len(expression) > i and expression[i] == ",":
         return int(expression[0:i]) + int(expression[i+1:len(expression)])
 
-    i = 3
+    i += 1
     if len(expression) > i and expression[i] == ",":
         return int(expression[0:i]) + int(expression[i+1:len(expression)])
 
