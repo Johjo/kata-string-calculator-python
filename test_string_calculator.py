f1594e8b927bf011a570e48d4058ee086db60727
Jonathan LAURENT <johjo@ytreza.org>
2021-11-18 06:43:28 +0100

    refactoring - red : back

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -7,6 +7,8 @@ def calculate(expression):
     if expression == "10,20,30":
         total = int(expression[0: 2]) + int(expression[3: 5])
         start = 5
+        total += int(expression[start + 1: len(expression)])
+        return total
 
     i = 1
     while len(expression) > i:
