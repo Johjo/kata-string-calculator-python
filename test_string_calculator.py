1e6a3c367c63ecf81d69572e8b78d1386f707383
Jonathan LAURENT <johjo@ytreza.org>
2021-11-18 05:42:05 +0100

    red - create test test_return_number_if_one_number_when_calculate

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,3 +4,7 @@ def calculate(expression):
 
 def test_return_zero_if_empty_string_when_calculate():
     assert calculate("") == 0
+
+
+def test_return_number_if_one_number_when_calculate():
+    assert calculate("54") == 54
