ed28bcffdf76a41a22d97664e9623de922c45e77
Jonathan LAURENT <johjo@ytreza.org>
2021-11-18 06:00:40 +0100

    red : add test to check if generic case work for comma at index 1 or 2

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -25,3 +25,5 @@ def test_return_number_if_one_number_when_calculate():
 def test_return_sum_if_two_numbers_when_calculate():
     assert calculate("12,124") == 136
     assert calculate("2,25") == 27
+    assert calculate("3,25") == 28
+    assert calculate("30,25") == 55
